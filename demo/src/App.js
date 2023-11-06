61bbf174a822e212c9f258262b97008bce5cc97a
baldbugulma <ForZaAriNdal@gmail.com>
2023-11-06 15:06:15 +0300

    2.5 Разделение приложения на модули

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { Book } from './Books';
+
+const App = () => { // Обратите внимание, что { на той же строке, где и return
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Book name="JS for Beginners" year="2018" price="1000" />
+      <Book name="11111" year="22222222" price="3333" />
+      <Book name="111" year="2018" price="2" />
     </div>
   );
-}
+};
 
 export default App;
+
