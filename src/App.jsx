0dab5939499f32a7739251dadda12c5800fcc9de
Konstantin <ForZaArindal@gmail.com>
2023-11-10 08:07:43 +0300

    3.7 Работа с коллекциями

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,25 @@
 import React, {Component} from 'react'
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      count: parseInt(localStorage.getItem('count')) || 0,
-      stop: true
+    state = {
+      posts: [
+        {id:'abc1', name: 'JS Basics'},
+        {id:'abc2', name: 'JS Advanced'},
+        {id:'abc3', name: 'React JS'},
+      ]
     };
-    this.handleClickStart = this.handleClickStart.bind(this);
-    this.handleClickStop = this.handleClickStop.bind(this);
 
-  }
-
-
-  handleClickStart (){
-    this.setState({stop: false});
-    const interval = setInterval(() => {
-      this.setState({count: this.state.count + 1});
-      localStorage.setItem('count', this.state.count+1)
-      if (this.state.stop === true){
-        clearInterval(interval);
-      }
-    },1000);
-
-  }
-
-  handleClickStop (){
-    this.setState({stop: true});
-  }
-
-
-
-
-  componentDidMount(){
-    console.log('componentDidMount');
-  }
-
-  componentDidUpdate(){
-    console.log('componentDidUpdate')
-    console.log(this.state.count)
-    console.log(this.state.stop)
-  }
-
-  componentWillUnmount(){
-    console.log('componentWillUnmount')
-  }
-  
-  render (){
-    console.log('render', this.state.count);
+  render() {
     return (
       <div className="App">
-        <p>{this.state.count}</p>
-        <button onClick={this.handleClickStart}>Start</button> 
-        <button onClick={this.handleClickStop}>Stop</button> 
+        {this.state.posts.map(post => (
+        <h2 key = {post.id}>{post.name}</h2>
+        ))}
       </div>
     );
   }
-
 }
+
+
 
 export default App;
