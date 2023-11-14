a35e8f48a3d6e0ed003fe60f92c56233ab11db15
Konstantin <ForZaArindal@gmail.com>
2023-11-14 07:53:46 +0300

    3.9 Задание на обновление состояния через дочерние компоненты

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,27 @@ import React, {Component} from 'react'
 import {Posts} from './components/Posts'
 
 class App extends Component {
-    state = {
+  state = {
       posts: [
-        {id:'abc1', name: 'JS Basics'},
-        {id:'abc2', name: 'JS Advanced'},
-        {id:'abc3', name: 'React JS'},
-      ]
-    };
+          { id: 'abc1', name: 'JS Basics' },
+          { id: 'abc2', name: 'JS Advanced' },
+          { id: 'abc3', name: 'Recat JS' },
+      ],
+  };
 
-    handleSomething = () => {
-      console.log('setState update')
-    }
+  removePost = (id) => {
+    this.setState({posts: this.state.posts.filter(post => post.id !== id)})
+  };
 
   render() {
-    return (
-      <div className="App">
-        <Posts posts = {this.state.posts} cb = {this.handleSomething}/>
-      </div>
-    );
+      const { posts } = this.state;
+
+      return (
+          <div className="App">
+              <Posts posts={posts} id = {posts.id} cb = {this.removePost}/>
+          </div>
+      );
   }
 }
-
-
 
 export default App;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
 export function Post (props){
-    return <h2 onClick = {props.cb}>{props.name}</h2>
+    const {name, id, cb} = props
+    return <h2>{name} <button onClick = {() => cb(id)}>Delete</button></h2>
 }
