b87e007064c678b24d2ee8adb182d120e64aec05
Konstantin <ForZaArindal@gmail.com>
2023-11-16 07:56:45 +0300

    4.3 Checkbox, radio button, select

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,31 +1,82 @@
-import React from 'react'
+import React from 'react';
 
-class Form extends React.Component{
-    state = {
-        firstName:'',
-        email: '',
-    }
+class Form extends React.Component {
+  state = {
+    firstName: '',
+    email: '',
+    message: '', 
+    select: '',
+    subscription:false,
+    gender: ''
+  };
 
-    handleChange = (event) => {
-        this.setState({[event.target.name]: event.target.value})
-    }
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-    render(){
-        const {firstName, email} = this.state;
-        return <div>
-            <input type="text" 
-            name = "firstName" 
-            placeholder="firstName"
-            value={firstName}
-            onChange={this.handleChange}/>
-            <input type="email" 
-            name = "email" 
-            placeholder="email"
-            value={email}
-            onChange={this.handleChange}/>
-        </div>
+  validateName = () => {
+    if (this.state.firstName.length < 5) {
+      alert('Введите больше символов в поле "Имя"');
     }
+  };
+
+  handleCheckBox = (event) =>{
+    this.setState({[event.target.name]: event.target.checked })
+  }
+
+  render() {
+    const { firstName, email, message, select, subscription } = this.state;
+    return (
+      <div>
+        <input
+          type="text"
+          name="firstName"
+          placeholder="Имя"
+          value={firstName}
+          onChange={this.handleChange}
+          onBlur={this.validateName}
+        />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={this.handleChange}
+          onBlur={this.validateName}
+        />
+
+        <select name="select" value={select} onChange={this.handleChange}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+        </select>
+
+        <br />
+
+        {/* Обновленная текстовая область (textarea) для корректной работы с полем message */}
+        <textarea
+          name="message"
+          value={message}
+          onChange={this.handleChange} // Добавляем обработчик изменения значения
+          onBlur={this.validateName} // Может потребоваться отрегулировать onBlur для textarea в соответствии с вашими требованиями
+        />
+
+        <input 
+        type="checkbox" 
+        name="subscription" 
+        checked={subscription}
+        onChange={this.handleCheckBox}
+        />
+
+        <br />
+
+        <input type="radio" name="gender" value="male" onChange={this.handleChange}/>Male
+        <input type="radio" name="gender" value="female" onChange={this.handleChange}/>Female
+      </div>
+      
+    );
+  }
 }
 
-
-export {Form};
+export { Form };
