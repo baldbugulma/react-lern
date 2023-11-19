b8c090acceb47c07dfc0fdd80626f242c2a16953
Konstantin <ForZaArindal@gmail.com>
2023-11-19 12:01:53 +0300

    4.4 Задание на создание формы - моя версия

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,82 +1,72 @@
 import React from 'react';
 
 class Form extends React.Component {
-  state = {
-    firstName: '',
+
+  initialState = {
     email: '',
-    message: '', 
-    select: '',
-    subscription:false,
-    gender: ''
+    subscription: false
+  };
+
+  state = {
+     ...this.initialState
+  };
+
+  resetForm = () => {
+    this.setState({ ...this.initialState }); // Сбрасываем состояние формы к исходным значениям
   };
 
   handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+    this.setState({[event.target.name]:event.target.value})
+  }
 
-  validateName = () => {
-    if (this.state.firstName.length < 5) {
-      alert('Введите больше символов в поле "Имя"');
+  handleCheckBox = (event) => {
+    this.setState({[event.target.name] : event.target.checked})
+  }
+
+  handleSend = () => {
+    if(this.state.email.length < 5){
+      alert('Некорректный эмейл')
     }
-  };
-
-  handleCheckBox = (event) =>{
-    this.setState({[event.target.name]: event.target.checked })
+    else if (this.state.subscription !== true){ 
+      alert('Нужно принять пользовательское соглашение')
+    }
+    else {
+      alert('Поздравляю с подпиской')
+      this.resetForm();
+    }
   }
 
   render() {
-    const { firstName, email, message, select, subscription } = this.state;
-    return (
+
+    const{subscription, email} = this.state;
+    return(
       <div>
-        <input
-          type="text"
-          name="firstName"
-          placeholder="Имя"
-          value={firstName}
-          onChange={this.handleChange}
-          onBlur={this.validateName}
-        />
-
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={email}
-          onChange={this.handleChange}
-          onBlur={this.validateName}
-        />
-
-        <select name="select" value={select} onChange={this.handleChange}>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-        </select>
-
-        <br />
-
-        {/* Обновленная текстовая область (textarea) для корректной работы с полем message */}
-        <textarea
-          name="message"
-          value={message}
-          onChange={this.handleChange} // Добавляем обработчик изменения значения
-          onBlur={this.validateName} // Может потребоваться отрегулировать onBlur для textarea в соответствии с вашими требованиями
-        />
-
         <input 
-        type="checkbox" 
-        name="subscription" 
-        checked={subscription}
-        onChange={this.handleCheckBox}
+        type="email" 
+        name="email"   
+        placeholder='Email'       
+        value={email}
+        onChange={this.handleChange}
         />
-
-        <br />
-
-        <input type="radio" name="gender" value="male" onChange={this.handleChange}/>Male
-        <input type="radio" name="gender" value="female" onChange={this.handleChange}/>Female
+        <br/>
+        <label>
+          <input type="checkbox" 
+          name="subscription"
+          checked={subscription}
+          onChange={this.handleCheckBox}
+          />
+          Я принимаю условия лицензионного соглашения
+        </label>
+        <br/>
+        <button onClick={this.handleSend}>Send</button>
       </div>
-      
-    );
+    )
+
+
   }
+
+
+
 }
 
 export { Form };
